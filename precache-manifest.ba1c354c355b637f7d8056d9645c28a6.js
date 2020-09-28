6096b185ad5fa86441c2bf2c6db762d800e3364b
Noam <noamru22@gmail.com>
2020-09-28 13:48:51 +0100

    Updates

---
+++ precache-manifest.ba1c354c355b637f7d8056d9645c28a6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a44b82b65f26c593d080a4a249dc7217",
+    "url": "/food-menu/index.html"
+  },
+  {
+    "revision": "4984188291a18abcd018",
+    "url": "/food-menu/static/css/main.ef148c56.chunk.css"
+  },
+  {
+    "revision": "42fbaf0a0202c71674b1",
+    "url": "/food-menu/static/js/2.24735acf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/food-menu/static/js/2.24735acf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4984188291a18abcd018",
+    "url": "/food-menu/static/js/main.6c910ff5.chunk.js"
+  },
+  {
+    "revision": "25cadc2a36f862f706a5",
+    "url": "/food-menu/static/js/runtime-main.bf0b1497.js"
+  },
+  {
+    "revision": "f8bd2a3330985a358421d30125e3fac4",
+    "url": "/food-menu/static/media/Rectangle 30.f8bd2a33.png"
+  },
+  {
+    "revision": "2b87af6632249acbaf92d7e7e483865f",
+    "url": "/food-menu/static/media/category-rec.2b87af66.png"
+  },
+  {
+    "revision": "bc64a769f52ddd3bd31830e20a033026",
+    "url": "/food-menu/static/media/classicburger.bc64a769.png"
+  }
+]);
